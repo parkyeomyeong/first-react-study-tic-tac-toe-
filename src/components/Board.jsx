b193dc49791bbd8b2a4c17e208a782c04fa9a02f
parkyeomyeong <tldldh1212@naver.com>
2022-04-05 19:54:51 +0900

    squares, xIsNext, handleClick function을 game component로 옮겨서 board와 squre을 모두 통제

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,51 +4,52 @@ import Square from './Squre';
 
 export default class Board extends React.Component {
     
-    constructor(props){
-        super(props);
-        // 일단 빙고를 판단하기 위해서도 array로 state를 관리해야징
-        this.state = {
-            squares : Array(9).fill(null),
-            xIsNext : true,
-        };
-    }
+    //아래 내용들을 상위 component인 Game에서 관리할거기 때문에 아래 constructor는 주석처리
+    // constructor(props){
+    //     super(props);
+    //     // 일단 빙고를 판단하기 위해서도 array로 state를 관리해야징
+    //     this.state = {
+    //         squares : Array(9).fill(null),
+    //         xIsNext : true,
+    //     };
+    // }
     
     
-    
-    handleClick(i){
-        //원본을 얕은복사해서 새로운 객체로 반환. 원본배열 바뀌지 않음~
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || this.state.squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-            this.setState({
-                squares : squares,
-                xIsNext : !this.state.xIsNext,
-        });
-    }
+    // Game component로 이동
+    // handleClick(i){
+    //     //원본을 얕은복사해서 새로운 객체로 반환. 원본배열 바뀌지 않음~
+    //     const squares = this.state.squares.slice();
+    //     if (calculateWinner(squares) || this.state.squares[i]){
+    //         return;
+    //     }
+    //     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //         this.setState({
+    //             squares : squares,
+    //             xIsNext : !this.state.xIsNext,
+    //     });
+    // }
 
     renderSquare(i) {
       return (
         <Square 
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
         />
         );
     }
 
     render(){
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if(winner){
-            status = `Winner : ${winner}`;
-        }else {
-            status = `Next Player : ${this.state.xIsNext ? 'X' : 'O'}`;
-        }
+        // const winner = calculateWinner(this.state.squares);
+        // let status;
+        // if(winner){
+        //     status = `Winner : ${winner}`;
+        // }else {
+        //     status = `Next Player : ${this.state.xIsNext ? 'X' : 'O'}`;
+        // }
 
         return (
             <div>
-                <div className='status'>{status}</div>
+                {/* <div className='status'>{status}</div> */}
                 <div className='borad-row'>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -69,22 +70,3 @@ export default class Board extends React.Component {
     }
   }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,17 +3,77 @@ import Board from './Board';
 // import "./mycss.css";
 
 export default class Game extends React.Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i){
+    //원본을 얕은복사해서 새로운 객체로 반환. 원본배열 바뀌지 않음~
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+        return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      //concat은 기존 배열을 변경하지 않아서 이렇게 함
+      history : history.concat([{
+        squares : squares,
+      }]),
+      xIsNext : !this.state.xIsNext,
+    });
+  }
+
+  render() {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const winner = calculateWinner(current.squares);
+      let status;
+      if(winner){
+        status = `Winner : ${winner}`;
+      }else{
+        status = `Next Player : ${(this.state.xIsNext ? 'X' : 'O')}`;
+      }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)} />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
       );
     }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
+}
